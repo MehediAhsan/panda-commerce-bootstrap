3aa136b55237858cd03dd14894c493095bd4f572
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-08-15 15:01:57 +0600

    simple modify js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// ২. যতগুলা ইলিমেন্টে h2 ট্যাগ আছে সেগুলাকে সিলেক্ট করে তার লেখাগুলার কালার lightblue করেদিতে পারো কিনা। 
+const allh2 = document.getElementsByTagName('h2');
+
+for(const h2 of allh2){
+    h2.style.color = 'lightblue';
+}
+
+//৩. ওই ওয়েবসাইট এ backpack আইডি দিয়ে একটা সেকশন আছে। সেই সেকশনের ব্যাকগ্রাউন্ড কালার tomato করে দিতে পারো কিনা। 
+document.getElementById('backpack').style.backgroundColor = 'tomato';
+
+//৪. card ক্লাস যতগুলা জায়গায় ইউজ করা হয়েছে সবগুলা কার্ড এর বর্ডার রেডিয়াস ৩০px করে দিতে পারো কিনা 
+const cards = document.getElementsByClassName('card');
+
+for(const card of cards){
+    card.style.borderRadius = '30px';
+}
+
+//৫. কোন একটা ফাংশন লিখো। সেটার ভিতরে কনসোল লগ হবে। এবং সেই ফাংশন টা ক্লিক হ্যান্ডলার হিসেবে কোন একটা বাটনে যোগ করো
+
+function onClick(){
+    console.log('button clicked');
+}
